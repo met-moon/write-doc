58604f641fee35aad80310c538fcc7d5066f84b5
heropoo <aiyouyou1000@163.com>
2018-01-22 14:20:22 +0800

    login

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -13,22 +13,10 @@ use Moon\Controller;
 class IndexController extends Controller
 {
     public function index(){
-        return 'welcome';
-    }
-
-    public function say(){
-        return [
-            'aa'=>'11'
-        ];
+        return 'welcome to write-doc';
     }
 
     public function login(){
-
-        /*$id = request('id', 1);
-
-        $user = new User();
-        $list = $user->where('id', $id)->get();
-        dd($id, $list->toArray());*/
         return $this->render('login');
     }
 }
