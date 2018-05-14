c3cf59bf2a817a9a1024f5c2af72e10a46d963bc
heropoo <aiyouyou1000@163.com>
2018-05-14 11:00:38 +0800

    builder

--- builder.php
+++ builder.php
@@ -14,25 +14,29 @@ $input = isset($_GET['input']) ? $_GET['input'] : 'index';
 $output = isset($_GET['output']) ? $_GET['output'] : $input;
 $layout = isset($_GET['layout']) ? $_GET['layout'] : 'main';
 
-$src = 'src/'.$input.'.md';
-$dst = 'dst/'.$output.'.html';
+$src_path = 'src';
+$dst_path = 'dst';
+$tmp_path = 'tmp';
+
+$src = $src_path.'/'.$input.'.md';
+$dst = $dst_path.'/'.$output.'.html';
 
 if(!file_exists($src)){
     throw new Exception('File `'.$src.'` is not exists!');
 }
 
-if(!file_exists('src/layouts/'.$layout.'.html')){
-    throw new Exception('File `src/layouts/'.$layout.'.html` is not exists!');
+if(!file_exists($src_path.'/layouts/'.$layout.'.html')){
+    throw new Exception('File `'.$src_path.'/layouts/'.$layout.'.html` is not exists!');
 }
 
 $content = file_get_contents($src);
 
 if(!empty($_GET['put_menu'])){  //放入目录列表
-    $dh = opendir('tmp');
+    $dh = opendir($tmp_path);
     $menu_src = '';
     while (($file = readdir($dh)) !== false) {
         if(strpos($file, '_menu_') === 0){
-            $menu_src .= file_get_contents('tmp/'.$file);
+            $menu_src .= file_get_contents($tmp_path.'/'.$file);
         }
     }
     closedir($dh);
@@ -43,7 +47,7 @@ $parser = new \cebe\markdown\GithubMarkdown();
 $parser->html5 = true;
 $content = $parser->parse($content);
 
-$html = file_get_contents('src/layouts/'.$layout.'.html');
+$html = file_get_contents($src_path.'/layouts/'.$layout.'.html');
 
 $html = preg_replace('#({{content}})#U', $content, $html);
 
@@ -51,26 +55,33 @@ if(!empty($_GET['get_menu'])){  //读取目录
     $dom = new DOMDocument();
     $dom->loadHTML($html);
     $menu = '';
-    $title = $dom->getElementsByTagName('h2')[0]->textContent;
+    $h2 = $dom->getElementsByTagName('h2')[0];
+    $title = $h2->textContent;
+    //$h2->setAttribute('id', urlencode($title));
+    $h2->setAttribute('id', base64_encode($title));
     $number = '';
     if(preg_match("#^([0-9\.]+)\s#", $title, $matches)){
-        $number = $matches[0];
+        $number = trim($matches[0]);
         $title = str_replace($number, '', $title);
     }
 
-    $menu .= strlen($number) > 0 ? $number.'['.$title.']('.$input.'.html)'.PHP_EOL : '* ['.$title.']('.$input.'.html)'.PHP_EOL;
+    $menu .= strlen($number) > 0 ? $number.' ['.$title.']('.$input.'.html)'.PHP_EOL : '* ['.$title.']('.$input.'.html)'.PHP_EOL;
 
     foreach($dom->getElementsByTagName('h3') as $h3){
-        $menu .= '    * ['.$h3->textContent.']('.$input.'.html#'.urlencode($h3->textContent).')'.PHP_EOL;
-        $h3->setAttribute('id', urlencode($h3->textContent));
+        //$menu .= '    * ['.$h3->textContent.']('.$input.'.html#'.urlencode($h3->textContent).')'.PHP_EOL;
+        //$h3->setAttribute('id', urlencode($h3->textContent));
+        $menu .= '    * ['.$h3->textContent.']('.$input.'.html#'.base64_encode($h3->textContent).')'.PHP_EOL;
+        $h3->setAttribute('id', base64_encode($h3->textContent));
     }
     $dom->saveHTMLFile($dst);
-    file_put_contents('tmp/_menu_'.$number.$input.'.md', $menu);
+    file_put_contents($tmp_path.'/_menu_'.$number.$input.'.md', $menu);
     $response =  file_get_contents($dst);
 }else{
     file_put_contents($dst, $html);
     $response =  $html;
 }
+
+$url = dirname($_SERVER['REQUEST_URI']).'/'.$input.'.html';
 
 ?><!doctype html>
 <html lang="en">
@@ -89,8 +100,8 @@ if(!empty($_GET['get_menu'])){  //读取目录
     </style>
 </head>
 <body>
-<div id="top_bar">文档地址：<a id="iframe_url" title="点击访问" target="_blank" href="dst/<?= $input?>.html">dst/<?= $input?>.html</a></div>
-<iframe src="dst/<?= $input?>.html" onload="loadFrame(this)"></iframe>
+<div id="top_bar">文档地址：<a id="iframe_url" title="点击访问" target="_blank" href="<?= $url?>"><?= $url?></a></div>
+<iframe src="<?=$url?>?v=<?= time()?>" onload="loadFrame(this)"></iframe>
 </body>
 <script>
     function loadFrame(obj){
