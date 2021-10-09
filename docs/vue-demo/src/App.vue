5a36154a83d520d8c0e954b8a622f1024c9889e6
heropoo <aiyouyou1000@163.com>
2021-10-09 15:18:01 +0800

    tes

--- docs/vue-demo/src/App.vue
+++ docs/vue-demo/src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import Element from './components/Element.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+//import Element from './components/Element.vue'
 export default {
   name: 'App',
   components: {
-    Element
+    //HelloWorld
   }
 }
 </script>
